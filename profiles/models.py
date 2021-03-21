d1f186a1435c820612abbde0e45c4952bf7f7bd4
Raúl <39159853+Raul14@users.noreply.github.com>
2021-03-21 14:04:00 +0100

    Profiles model created.

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    #Functionality keys:
+    owner = models.OneToOneField(
+                User,
+                on_delete=models.CASCADE
+            )
+    avatar = models.ImageField(
+                upload_to='avatars',
+                default='avatar.png',
+                blank=False
+            )
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    following = models.ManyToManyField(
+                User,
+                related_name='self_following',
+                blank=True
+            )
+
+    #Content keys:
+    bio = models.TextField(default="There's no bio so far.")
+
+    ##METHODS:
+
+    def __str__(self):
+        return str(self.owner)
+
+    #................................................................
