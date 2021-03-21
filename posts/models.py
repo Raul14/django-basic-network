75cda0c5ca7ee2f61a9deb9bbb76c7181cf566b0
Raúl <39159853+Raul14@users.noreply.github.com>
2021-03-21 13:53:51 +0100

    Posts model created.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    #Functionality keys:
+    author = models.ForeignKey(
+                Profile,
+                on_delete=models.CASCADE,
+                related_name='self_posts',
+                null=True
+            )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    liked = models.ManyToManyField(
+                User,
+                default=None,
+                blank=True
+            )
+    
+    #Content keys:
+    body = models.TextField()
+    picture = models.ImageField(
+                upload_to='images',
+                blank=True
+            )
+
+    ##METHODS:
+
+    def __str__(self):
+        return str(self.pk) #Research about this primary key inheritance
+                            # from models.Model
+
+    #................................................................
