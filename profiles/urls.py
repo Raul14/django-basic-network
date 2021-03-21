9036635fc860870bb7967cf4b9e99539f21cca98
Raúl <39159853+Raul14@users.noreply.github.com>
2021-03-21 13:21:06 +0100

    Posts/Profiles application urls.py file template. Further improvements to come.

---
+++ profiles/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+#from .views import <views_to_import> ...
+
+app_name = 'posts'
+
+urlpatterns = [
+    #path('', home_view, name='posts-home-view'),  #Example home_view
+]
