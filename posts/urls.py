b152b42eca8a5a4f400a8d825d542dce7261a4a0
Raúl <39159853+Raul14@users.noreply.github.com>
2021-03-22 19:28:06 +0100

    First path for the main (mocked) view added

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
+from .views import home_view
 #from .views import <views_to_import> ...
 
 app_name = 'posts'
 
 urlpatterns = [
-    #path('', home_view, name='posts-home-view'),  #Example home_view
+    path('', home_view, name='posts-home-view'),  #Example home_view
 ]
