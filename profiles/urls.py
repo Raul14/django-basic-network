fed217fdad7b4a4ed7c56421ffb39394f8d14c93
Raúl <39159853+Raul14@users.noreply.github.com>
2021-03-23 17:46:32 +0100

    Set login required to access the main profile view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
+from .views import profiles_home_view
 #from .views import <views_to_import> ...
 
 app_name = 'profiles'
 
 urlpatterns = [
-    #path('', home_view, name='profiles-home-view'),  #Example home_view
+    path('', profiles_home_view, name='profiles-home-view'),  #Example home_view
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.ç
+@login_required(redirect_field_name='', login_url='/admin')
+def profiles_home_view(request):
+    return render(request, 'profile-home.html', {}) #This will be the main page for the service
